4ab448d055ea0e1396c5ffba491980aa3fbc5539
hannamkrtchian <104131383+hannamkrtchian@users.noreply.github.com>
2023-01-11 19:26:40 +0100

    connected with database & first find function

---
+++ database/connection.js
@@ -0,0 +1,10 @@
+// Connect with environment.
+require('dotenv').config();
+
+const { MongoClient } = require('mongodb');
+
+const uri = process.env.MONGO_URI;
+
+const client = new MongoClient(uri);
+
+module.exports = client;
--- routes/index.js
+++ routes/index.js
@@ -1,9 +1,39 @@
 var express = require('express');
 var router = express.Router();
+const client = require('../database/connection');
+
+// find all posts function
+async function findAllPosts(client) {
+  const cursor = client.db("forum").collection("posts").find();
+
+  const results = await cursor.toArray();
+
+  if (results.length > 0) {
+      return results;
+  } else {
+      return "No posts yet.";
+  }
+}
 
 /* GET home page. */
-router.get('/', function(req, res, next) {
-  res.render('index', { title: 'Forum' });
+router.get('/', async function(req, res, next) {
+  try {
+    // connect & check
+    await client.connect();
+    console.log("Connected to MongoDB on /");
+
+    // get all posts
+    let posts = await findAllPosts(client);
+
+    // send posts to view
+    res.render('index', { title: 'Forum', posts: posts });
+
+} catch (e) {
+    console.error(e);
+} finally {
+    // close connection
+    await client.close();
+}
 });
 
 module.exports = router;
